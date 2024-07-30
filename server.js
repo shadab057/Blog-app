0586799a41f4327effebabf453d0e9a7f845830c
shadab057 <mohdshadab4629@gmail.com>
2024-07-30 19:01:21 +0530

    Blog App Setup And Creating Node Server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require('cors')
+const morgan = require ('morgan')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+
+// env config
+dotenv.config({ 
+})
+
+// rest object 
+const app = express()
+
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        "message":"Node server"
+    })
+})
+
+// Port
+const PORT = process.env.PORT || 8080 ;
+
+//listing
+app.listen(PORT, () => {
+    console.log(
+      `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+        .white
+    );
+  });
